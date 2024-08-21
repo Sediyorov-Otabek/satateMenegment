9957fe0aefb4a33ad4e15a8eb1ff5e20b6abc983
Пользователь <otabeksediyorov298@gmail.com>
2024-08-21 13:00:17 +0500

    vite logo add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import logo from "@/assets/logo-removebg-preview.png";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className=" bg-slate-100 h-20">
       <div className="h-full flex gap-8 items-center container mx-auto">
-        <NavLink className="text-2xl flex-1" to={"/"}>
-          Logoo
+        <NavLink
+          className="text-2xl flex-1 w-10 h-10 object-contain flex items-center justify-start"
+          to={"/"}
+        >
+          <div>
+            <img className="w-36 h-16 " src={logo} alt="" />
+          </div>
         </NavLink>
         <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/wishlist"}>Wishlist</NavLink>
